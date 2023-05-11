91e36f72aa8db2a2e3d0bc54e3f7c17fb270adfc
Ryan Gainor <rmgainor@gmail.com>
2023-05-11 10:07:12 -0500

    initial commit

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -11,13 +11,13 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"Lamborghini","Malibu Mansion","Super Yacht"}; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
@@ -28,7 +28,7 @@ namespace ChristmasWithTypes
                 Console.WriteLine($"    {present}");
             }
 
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            Console.WriteLine($"\n We like to call John, {xmas.Santa}");
         }
     }
 }
